a79a0fa5ab82dd1610cd029b799b2dc5526f071e
Paul <dotdotdotpaul@gmail.com>
2017-06-02 12:14:33 -0700

    Scaffold

---
+++ lib/expublica.ex
@@ -0,0 +1,18 @@
+defmodule ExPublica do
+  @moduledoc """
+  Documentation for ExPublica.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPublica.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/expublica_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPublicaTest do
+  use ExUnit.Case
+  doctest ExPublica
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
